0e4c7fbbb45889891fabc908594168a23fc1ca41
Miriam Zednickova <mitcamiit@gmail.com>
2025-05-05 13:23:29 +0200

    fix sidebar items length

--- frontend/src/layout/SIdebarMenu.tsx
+++ frontend/src/layout/SIdebarMenu.tsx
@@ -1,58 +1,58 @@
-import { Box, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Stack } from "@mui/material";
-import { useLocation, useNavigate } from "react-router-dom";
+import {Box, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Stack} from "@mui/material";
+import {useLocation, useNavigate} from "react-router-dom";
 import StorageIcon from "@mui/icons-material/Storage";
 import BarChartIcon from "@mui/icons-material/BarChart";
 import SwapHorizIcon from "@mui/icons-material/SwapHoriz";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
-import { ROUTES } from "../constants/routes";
-import { CLEVERMINER_DOCS_URL } from "../constants/constants";
-import { MenuItem, MenuItemType } from "./MenuItem";
-import { Colors } from "../styles/colors";
+import {ROUTES} from "../constants/routes";
+import {CLEVERMINER_DOCS_URL} from "../constants/constants";
+import {MenuItem, MenuItemType} from "./MenuItem";
+import {Colors} from "../styles/colors";
 
 type GroupType = {
-  items: MenuItemType[];
+    items: MenuItemType[];
 };
 
 // Menu Data
 const menuGroups: GroupType[] = [
-  {
-    items: [{ name: "Data & Preprocessing", icon: <StorageIcon />, path: ROUTES.DATASETS }],
-  },
-  {
-    items: [
-      // todo: put names into constants
-      { name: "CF Miner", icon: <BarChartIcon />, path: ROUTES.CF_MINER },
-      { name: "4ft Miner", icon: <SwapHorizIcon />, path: ROUTES["4FT_MINER"] },
-    ],
-  },
-  {
-    items: [{ name: "Documentation", icon: <HelpOutlineIcon />, path: CLEVERMINER_DOCS_URL }],
-  },
+    {
+        items: [{name: "Data & Preprocessing", icon: <StorageIcon/>, path: ROUTES.DATASETS}],
+    },
+    {
+        items: [
+            // todo: put names into constants
+            {name: "CF Miner", icon: <BarChartIcon/>, path: ROUTES.CF_MINER},
+            {name: "4ft Miner", icon: <SwapHorizIcon/>, path: ROUTES["4FT_MINER"]},
+        ],
+    },
+    {
+        items: [{name: "Documentation", icon: <HelpOutlineIcon/>, path: CLEVERMINER_DOCS_URL}],
+    },
 ];
 
 // Sidebar Menu Component
 export const SidebarMenu = () => {
-  const location = useLocation();
-  const currentPath = location.pathname;
+    const location = useLocation();
+    const currentPath = location.pathname;
 
-  return (
-    <Stack sx={{ minWidth: 300, borderRight: `1px solid ${Colors.border}`, bgcolor: Colors.sidebar }}>
-      {menuGroups.map((group, index) => (
-        <Stack direction={"row"} key={index}>
-          <List>
-            {group.items.map((item, idx) => (
-              <MenuItem
-                key={idx}
-                name={item.name}
-                icon={item.icon}
-                path={item.path}
-                selected={currentPath === item.path}
-              />
+    return (
+        <Stack sx={{minWidth: 300, borderRight: `1px solid ${Colors.border}`, bgcolor: Colors.sidebar}}>
+            {menuGroups.map((group, index) => (
+                <Stack direction={"row"} key={index}>
+                    <List sx={{flexGrow: 1}}>
+                        {group.items.map((item, idx) => (
+                            <MenuItem
+                                key={idx}
+                                name={item.name}
+                                icon={item.icon}
+                                path={item.path}
+                                selected={currentPath === item.path}
+                            />
+                        ))}
+                    </List>
+                    {index < menuGroups.length - 1 && <Divider/>}
+                </Stack>
             ))}
-          </List>
-          {index < menuGroups.length - 1 && <Divider />}
         </Stack>
-      ))}
-    </Stack>
-  );
+    );
 };
